752558c5c1130c6bf0956daf9a669786c44d09db
Spencer Owen <bubbleboy450@gmail.com>
2013-03-24 14:16:31 -0600

    pachubeTest now smooths it's data before uploading to cosm:

--- pachubeTest/pachubeTest.ino
+++ pachubeTest/pachubeTest.ino
@@ -36,11 +36,17 @@ byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0x44, 0x44 };
 // Analog pin which we're monitoring (0 and 1 are used by the Ethernet shield)
 int sensorPin = 2;
 
-
 unsigned long lastConnectionTime = 0;                // last time we connected to Cosm
-//const unsigned long connectionInterval = 15000;      // delay between connecting to Cosm in milliseconds
+const unsigned long connectionInterval = 15000;      // delay between connecting to Cosm in milliseconds
 /* Increase frequency of updates from 15 seconds to .5 seconds (for testing)*/
-const unsigned long connectionInterval = 500;      // delay between connecting to Cosm in milliseconds
+//const unsigned long connectionInterval = 500;      // delay between connecting to Cosm in milliseconds
+
+//Kocks don't always happen right when the knock sensor is polled. Causes readings to show 0 more often than it should
+const int numberOfReadings = 30; 
+int readings[numberOfReadings]; // array of all analog reads
+int readingsIndex = 0;
+int readingsTotal = 0;
+int readingsAverage = 0;
 
 
 // Initialize the Cosm library
@@ -59,6 +65,7 @@ EthernetClient client;
 CosmClient cosmclient(client);
 
 void setup() {
+ 
   // put your setup code here, to run once:
   Serial.begin(115200);
 
@@ -73,20 +80,42 @@ void setup() {
 
   Serial.println("Network initialized");
   Serial.println();
+  
+  //Fill the array of sensor readings with 0's 
+  for (int thisReading = 0; thisReading < numberOfReadings; thisReading++ ){
+    readings[thisReading] = 0;
+  }
 }
 
 void loop() {
   // main program loop
   if (millis() - lastConnectionTime > connectionInterval) {
     // read a value from the pin
-    int sensorValue = analogRead(sensorPin);
+    ////int sensorValue = analogRead(sensorPin);
     // send it to Cosm
-    sendData(sensorValue);
+    //sendData(sensorValue);
+    sendData(readingsAverage);
     // read the datastream back from Cosm
     //getData();
     // update connection time so we wait before connecting again
     lastConnectionTime = millis();
   }
+  //Subtract the last reading
+  readingsTotal = readingsTotal - readings[readingsIndex];
+  readings[readingsIndex] = analogRead(sensorPin);
+  readingsTotal = readingsTotal + readings[readingsIndex];
+  readingsIndex = readingsIndex + 1;
+
+  //if at the end of array, wrap to begining
+  if (readingsIndex >= numberOfReadings) {
+      readingsIndex = 0;
+  }
+  //Calculate the average reading in the array
+  readingsAverage = readingsTotal / numberOfReadings;
+
+  Serial.print("Sensor = ");
+  Serial.println( analogRead(sensorPin) );
+  delay(100);
 }
 
 // send the supplied value to Cosm, printing some debug information as we go
@@ -97,7 +126,8 @@ void sendData(int sensorValue) {
   Serial.println(datastreams[0].getFloat());
   Serial.println(sensorValue);
 
-  Serial.println("Uploading to Cosm");
+  Serial.print("Uploading to Cosm ");
+  Serial.println(sensorValue);
   int ret = cosmclient.put(feed, API_KEY);
   Serial.print("PUT return code: ");
   Serial.println(ret);
