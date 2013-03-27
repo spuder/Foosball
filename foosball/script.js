b316c85316a2752531f1cfbb0fd5620808516bf4
Spencer Owen <bubbleboy450@gmail.com>
2013-03-27 00:37:18 -0600

    Includes html5 and mobile sub webpagescss circle json updates text in circle

--- foosball/script.js
+++ foosball/script.js
@@ -1,14 +1,28 @@
 $(document).ready( function()  {
-    $('div').mouseenter(function() {
-        $('div').fadeTo('fast',1);
-    });
-    $('div').mouseleave(function() {
-        $('div').fadeTo('fast', 0.10);
-    });
-    $('#green').mouseenter(function() {
-        $('div').fadeTo('fast',1);
-    });
-    $('#green').mouseleave(function() {
-        $('div').fadeTo('fast', 0.10);
-    });
+    var occupied = false;
+    var feed_120687 = "https://api.cosm.com/v2/feeds/120687.json";
+
+    $.getJSON( feed_120687 ,function(data){alert(data.msg); });
+
+    var contentDiv = $('.tableStatus').html();
+    $('.tableStatus').append( occupied.toString() );
+    
+
+  //    $(function() {
+  //   $( "#graph" ).accordion();
+  // });
+    // $('div').mouseenter(function() {
+    //     $('div').fadeTo('fast',1);
+    // });
+    //  $('div').mouseleave(function() {
+    //     $('div').fadeTo('fast', 0.10);
+    // });
 });
+
+/*
+http://www.jquery4u.com/dynamic-css-2/change-css-jquery/
+//change all elements with class .bg-red to have a red background
+$('.bg-red').css({"background-color":"red"});
+*/
+
+//https://api.cosm.com/v2/feeds/120687.json
