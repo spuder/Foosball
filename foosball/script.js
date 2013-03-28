6d698e205e0e7b88c061ad5b86e281ae1b7e4022
Spencer Owen <bubbleboy450@gmail.com>
2013-03-27 21:10:19 -0600

    JSON is correctly parsed if you are logged into cosm

--- foosball/script.js
+++ foosball/script.js
@@ -1,28 +1,55 @@
 $(document).ready( function()  {
     var occupied = false;
-    var feed_120687 = "https://api.cosm.com/v2/feeds/120687.json";
+    var arrayOfReadings = [0,0,0,0,0,0,0,0,0,0];
+    var arrayOfReadingsIndex = 0;
+    
+    getJson();
+    //setInterval (getJson, 1000);
 
-    $.getJSON( feed_120687 ,function(data){alert(data.msg); });
+    //searchReadings(42);
+    //alert(searchReadings(42));
 
     var contentDiv = $('.tableStatus').html();
     $('.tableStatus').append( occupied.toString() );
     
+    $('div').mouseenter(function() {
+        $('div').fadeTo('fast',1);
+        getJson("foosball_key");
+    });
 
-  //    $(function() {
-  //   $( "#graph" ).accordion();
-  // });
-    // $('div').mouseenter(function() {
-    //     $('div').fadeTo('fast',1);
-    // });
-    //  $('div').mouseleave(function() {
-    //     $('div').fadeTo('fast', 0.10);
-    // });
 });
 
-/*
-http://www.jquery4u.com/dynamic-css-2/change-css-jquery/
-//change all elements with class .bg-red to have a red background
-$('.bg-red').css({"background-color":"red"});
-*/
-
 //https://api.cosm.com/v2/feeds/120687.json
+
+//curl --request GET --header "X-ApiKey: -Ux_JTwgP-8pje981acMa5811-mSAKxpR3VRUHRFQ3RBUT0g" https://api.cosm.com/v2/feeds/120687/datastreams/sensor_reading
+
+function getJson() {
+    //$('#feed').html('<span><img src="/blog/images/lightbox-ico-loading.gif" alt=""></span>');
+    $.ajax({
+        type:'GET',
+        url:"https://api.cosm.com/v2/feeds/120687/datastreams/sensor_reading",
+        //data:"id="+flickrid+"&lang=en-us&format=json&jsoncallback=?",
+        data:"X-ApiKey: -Ux_JTwgP-8pje981acMa5811-mSAKxpR3VRUHRFQ3RBUT0g",
+        success:function(feed) {
+         // alert(feed.current_value);
+          $('#rawData').html(feed.current_value);
+        },
+        dataType:'jsonp'
+    });
+}
+
+function addReading(reading) {
+  arrayOfReadings.insert(arrayOfReadingsIndex, reading);
+  arrayOfReadingsIndex++;
+
+  if (arrayOfReadingsIndex >= arrayOfReadings.length) {
+    arrayOfReadingsIndex = 0;
+  }
+  return;
+}
+
+function searchReadings(reading) {
+
+  return arrayOfReadings.indexOf(reading);
+
+}
