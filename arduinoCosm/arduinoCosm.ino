dfb35f07336a9bf516ff17d25b08ec3040069a90
Spencer Owen <spencerowen@Spencers-MacBook-Pro.local>
2013-04-06 00:42:50 -0600

    Version 2.0 Complete Rewrite to make more Cohesive, and less tightly coupled

--- arduinoCosm/arduinoCosm.ino
+++ arduinoCosm/arduinoCosm.ino
@@ -12,6 +12,15 @@ Full tutorial available here: https://cosm.com/docs/quickstart/arduino.html
  *
  * This code is in the public domain.
  */
+ 
+ /*
+ Revision 2.0
+ 2013-April-06
+ spuder
+   -Adds new functions purgeArray(), getAverage(), addReading()
+   -Complete rewrite of the code to make it more cohesive and less tightly coupled
+   -Increases the frequency of data uploads to every 1.5 seconds
+ */
 
 #include <SPI.h>
 #include <Ethernet.h>
@@ -22,50 +31,34 @@ Full tutorial available here: https://cosm.com/docs/quickstart/arduino.html
 /*********************************************************************************
 **********************************************************************************
 **
-** DO NOT USE THIS API KEY!!
+** DO NOT USE THESE VALUES!
 ** Instead go to cosm, create your own account, and place your key here:
 **
 **********************************************************************************
 **********************************************************************************/
-#define API_KEY "1QrIEwOdd5le4oNFrCtgGti9dEuSAKxobmlwQjRsd21CST0g" // Spencers Key, Do not use!!!
-//#define API_KEY "<replace_with_your_key>"                        // your Cosm API key
+#define API_KEY "1QrIEwOdd5le4oNFrCtgGti9dEuSAKxobmlwQjRsd21CST0g" 
+#define FEED_ID 120687                  
+char sensorId[] = "sensor_reading"; //Datastream
 
-/*********************************************************************************
-**********************************************************************************
-**
-** DO NOT USE THIS FEED ID!!
-** Instead go to cosm, create your own account, and place your ID here:
-**
-**********************************************************************************
-**********************************************************************************/
-#define FEED_ID 120687                  // Spencers ID, DO NOT USE
-//#define FEED_ID replace_with_your_feed_id // your Cosm feed ID
+
+
 
 
 
 // MAC address for your Ethernet shield
 byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0x44, 0x44 };
 
-// Analog pin which we're monitoring (0 and 1 are used by the Ethernet shield)
+// Analog pin connected to sensor (0 and 1 are used by the Ethernet shield)
 int sensorPin = 2;
 
 unsigned long lastConnectionTime = 0;                // last time we connected to Cosm
-//const unsigned long connectionInterval = 15000;      // delay between connecting to Cosm in milliseconds
-/* Increase frequency of updates from 15 seconds to .5 seconds (for testing)*/
 const unsigned long connectionInterval = 1500;      // delay between connecting to Cosm in milliseconds
 
 //Kocks don't always happen right when the knock sensor is polled. Causes readings to show 0 more often than it should
-const int numberOfReadings = 30; 
+const int numberOfReadings = 15; //recomended that connectionInterval and numberOfReadings are multiples of each other
 int readings[numberOfReadings]; // array of all analog reads
 int readingsIndex = 0;
-int readingsTotal = 0;
-int readingsAverage = 0;
 
-
-// Initialize the Cosm library
-
-// Define the string for our datastream ID
-char sensorId[] = "sensor_reading";
 
 CosmDatastream datastreams[] = {
   CosmDatastream(sensorId, strlen(sensorId), DATASTREAM_FLOAT),
@@ -94,45 +87,67 @@ void setup() {
   Serial.println("Network initialized");
   Serial.println();
   
-  //Fill the array of sensor readings with 0's 
-  for (int thisReading = 0; thisReading < numberOfReadings; thisReading++ ){
-    readings[thisReading] = 0;
-  }
+  //Set all values of array to 0
+  purgeArray();
 }
 
 void loop() {
   // main program loop
   if (millis() - lastConnectionTime > connectionInterval) {
-    // read a value from the pin
-    ////int sensorValue = analogRead(sensorPin);
-    // send it to Cosm
-    //sendData(sensorValue);
-    sendData(readingsAverage);
-    readingsAverage=0;//reset the average
-    // read the datastream back from Cosm
-    //getData();
+    sendData( getAverage() );
+    purgeArray();
     // update connection time so we wait before connecting again
     lastConnectionTime = millis();
   }
-  //Subtract the last reading
-  readingsTotal = readingsTotal - readings[readingsIndex];
-  readings[readingsIndex] = analogRead(sensorPin);
-  readingsTotal = readingsTotal + readings[readingsIndex];
-  readingsIndex = readingsIndex + 1;
-
-  //if at the end of array, wrap to begining
-  if (readingsIndex >= numberOfReadings) {
-      readingsIndex = 0;
-  }
-  //Calculate the average reading in the array
-  readingsAverage = readingsTotal / numberOfReadings;
+  int theReading = analogRead( sensorPin );
+  addReading( theReading ); //Adds the reading to the array
 
   Serial.print("Sensor = ");
-  Serial.println( analogRead(sensorPin) );
+  Serial.println( theReading );
   delay(100);
 }
 
-// send the supplied value to Cosm, printing some debug information as we go
+/**
+  Fills the entire array with 0's
+*/
+void purgeArray() {
+    //Fill the array of sensor readings with 0's 
+  for (int thisReading = 0; thisReading < numberOfReadings; thisReading++ ){
+    readings[thisReading] = 0;
+  }
+  readingsIndex=0;
+}
+  
+/**
+  Appends the most recent sensor reading to the array of readings
+  Array list is defined by "numberOfReadings*
+*/
+void addReading(int aReading) {
+  readings[readingsIndex] = aReading;
+  readingsIndex = readingsIndex + 1;
+  if ( readingsIndex >= numberOfReadings ) {
+    readingsIndex = 0;
+  }
+}
+
+/**
+  Returns the average value of the array
+  See arduino "smoothing" example for more info
+  Usefull if your sensor readings are too far appart to 
+    always catch the trigger
+*/
+int getAverage() {
+  int readingsTotal = 0;
+  for (int i = 0; i < numberOfReadings; i++ ) {
+    readingsTotal = readingsTotal + readings[i];
+  }
+  return readingsTotal / numberOfReadings ;
+  
+}
+
+/**
+  Send the reading Cosm, printing some debug information as we go
+*/
 void sendData(int sensorValue) {
   datastreams[0].setFloat(sensorValue);
 
@@ -149,7 +164,10 @@ void sendData(int sensorValue) {
   Serial.println();
 }
 
-// get the value of the datastream from Cosm, printing out the value we received
+/*
+  Get the value of the datastream from Cosm, printing out the value we received
+  Not used in this particular sketch
+ */
 void getData() {
   Serial.println("Reading data from Cosm");
 
